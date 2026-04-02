8a7dad24ddb962128796209eb69186f671416cbd
Blanchard <blanchardd@CSGP.EDU>
2026-04-02 08:18:11 -0600

    Update to Graphs

--- graph.py
+++ graph.py
@@ -1,29 +1,38 @@
 import matplotlib.pyplot as plot
 import numpy as npy
 
-voltage = [(0.1 * num) for num in range(0, 50)]
-power = []
-current = []
+def create_example_graphs():
+    voltage = [(0.1 * num) for num in range(0, 50)]
+    power = []
+    current = []
 
-for num in voltage:
-    power.append(-0.01 * (num ** 5) + 0.65 * (num ** 2))
-    current.append(-1 * (8 ** (num - 3.6)) + 2.5)
+    for num in voltage:
+        power.append(-0.01 * (num ** 5) + 0.65 * (num ** 2))
+        current.append(-1 * (8 ** (num - 3.6)) + 2.5)
+    current.pop()
+    current.append(0)
+    
+    return voltage, power, current
 
-plot.figure(figsize = (10, 8))
-plot.plot(voltage, power)
-plot.plot(voltage, current)
+def graph_data(voltage, power, current):
+    max_pow = npy.max(power)
+    max_ind = power.index(max_pow)
+    max_volt = voltage[max_ind]
+    max_curr = current[max_ind]
 
-plot.grid(True, alpha = 0.3, linestyle = ':')
-plot.ylim(0, 4)
-plot.xlim(0, 5)
+    zero = current.index(0) * 0.1
 
-max_pow = npy.max(power)
-print(max_pow)
-max_ind = power.index(max_pow)
-print(max_ind)
-max_volt = voltage[max_ind]
-print(max_volt)
-max_curr = current[max_ind]
-print(max_curr)
+    plot.figure(figsize = (10, 8))
+    plot.plot(voltage, power)
+    plot.plot(voltage, current)
 
-plot.show()
+    plot.grid(True, alpha = 0.3, linestyle = ':')
+    plot.ylim(0, max_pow)
+    plot.xlim(0, zero)
+
+    plot.show()
+
+    return max_pow, max_volt, max_curr
+
+volt, powr, curr = create_example_graphs()
+print(graph_data(volt, powr, curr))
